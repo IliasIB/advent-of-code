55207d94005ee5a22644444d27fe6f9322a0581d
Ilias Bachiri <ilias.bachiri@gmail.com>
2024-12-22 15:18:25 +0100

    Day 22 and partial day 21

--- src/year2024/day21.rs
+++ src/year2024/day21.rs
@@ -1,3 +1,135 @@
+fn parse(contents: &str) -> Vec<&str> {
+    return contents.lines().collect::<Vec<&str>>();
+}
+
+fn get_numeric_horizontal(number: &char) -> isize {
+    return match number {
+        '7' | '4' | '1' => 0,
+        '8' | '5' | '2' | '0' => 1,
+        '9' | '6' | '3' | 'A' => 2,
+        _ => unreachable!(),
+    };
+}
+
+fn get_numeric_vertical(number: &char) -> isize {
+    return match number {
+        '7' | '8' | '9' => 0,
+        '4' | '5' | '6' => 1,
+        '1' | '2' | '3' => 2,
+        '0' | 'A' => 3,
+        _ => unreachable!(),
+    };
+}
+
+fn get_directional_horizontal(number: &char) -> isize {
+    return match number {
+        '<' => 0,
+        '^' | 'v' => 1,
+        '>' | 'A' => 2,
+        _ => unreachable!(),
+    };
+}
+
+fn get_directional_vertical(number: &char) -> isize {
+    return match number {
+        '^' | 'A' => 0,
+        '<' | 'v' | '>' => 1,
+        _ => unreachable!(),
+    };
+}
+
+fn convert_horizontal(amount: isize) -> String {
+    if amount > 0 {
+        return ">".repeat(amount.abs() as usize);
+    } else if amount < 0 {
+        return "<".repeat(amount.abs() as usize);
+    } else {
+        return "".to_string();
+    }
+}
+
+fn convert_vertical(amount: isize) -> String {
+    if amount > 0 {
+        return "v".repeat(amount.abs() as usize);
+    } else if amount < 0 {
+        return "^".repeat(amount.abs() as usize);
+    } else {
+        return "".to_string();
+    }
+}
+
+fn get_numeric_sequence(start: &char, end: &char) -> String {
+    let vertical = get_numeric_vertical(end) - get_numeric_vertical(start);
+    let horizontal = get_numeric_horizontal(end) - get_numeric_horizontal(start);
+
+    if vertical > 0 {
+        return convert_horizontal(horizontal) + &convert_vertical(vertical);
+    } else {
+        return convert_vertical(vertical) + &convert_horizontal(horizontal);
+    }
+}
+
+fn get_directional_sequence(start: &char, end: &char) -> String {
+    let vertical = get_directional_vertical(end) - get_directional_vertical(start);
+    let horizontal = get_directional_horizontal(end) - get_directional_horizontal(start);
+
+    if vertical < 0 {
+        return convert_horizontal(horizontal) + &convert_vertical(vertical);
+    } else {
+        return convert_vertical(vertical) + &convert_horizontal(horizontal);
+    }
+}
+
+fn part1(codes: &Vec<&str>) {
+    let mut sequences1: Vec<String> = Vec::new();
+    let mut sequences2: Vec<String> = Vec::new();
+    let mut sequences3: Vec<String> = Vec::new();
+
+    let mut current_numeric = 'A';
+    let mut current_directional1 = 'A';
+    let mut current_directional2 = 'A';
+    for &code in codes {
+        let mut numeric_sequence: String = "".to_string();
+        for button in code.chars() {
+            numeric_sequence.push_str(&get_numeric_sequence(&current_numeric, &button));
+            numeric_sequence.push('A');
+            current_numeric = button;
+        }
+        sequences1.push(numeric_sequence.clone());
+
+        let mut directional_sequence1: String = "".to_string();
+        for button in numeric_sequence.chars() {
+            directional_sequence1
+                .push_str(&get_directional_sequence(&current_directional1, &button));
+            directional_sequence1.push('A');
+            current_directional1 = button;
+        }
+        sequences2.push(directional_sequence1.clone());
+
+        let mut directional_sequence2: String = "".to_string();
+        for button in directional_sequence1.chars() {
+            directional_sequence2
+                .push_str(&get_directional_sequence(&current_directional2, &button));
+            directional_sequence2.push('A');
+            current_directional2 = button;
+        }
+
+        sequences3.push(directional_sequence2);
+    }
+
+    for i in 0..sequences1.len() {
+        println!("{}", sequences1[i]);
+        println!("{}", sequences2[i]);
+        println!("{}", sequences3[i]);
+        println!(
+            "{} * {}",
+            sequences3[i].len(),
+            codes[i][0..3].parse::<usize>().unwrap()
+        );
+    }
+}
+
 pub fn day21(contents: &str) {
-    todo!()
+    let codes = parse(contents);
+    part1(&codes);
 }
--- src/year2024/day22.rs
+++ src/year2024/day22.rs
@@ -1,3 +1,117 @@
+use std::collections::HashMap;
+
+fn parse(contents: &str) -> Vec<usize> {
+    return contents
+        .lines()
+        .map(|line| line.parse::<usize>().unwrap_or_default())
+        .collect();
+}
+
+fn mix_and_prune(secret_number: &mut usize, new_number: usize) {
+    *secret_number = *secret_number ^ new_number;
+    *secret_number = *secret_number % 16777216;
+}
+
+fn generate_secret_number(seed: &usize, cache: &mut HashMap<usize, usize>) -> usize {
+    let mut secret_number = *seed;
+    let mut new_number: usize;
+    for _ in 0..2000 {
+        if let Some(number) = cache.get(&secret_number) {
+            secret_number = *number;
+            continue;
+        }
+        let initial_number = secret_number;
+
+        new_number = secret_number * 64;
+        mix_and_prune(&mut secret_number, new_number);
+
+        new_number = secret_number / 32;
+        mix_and_prune(&mut secret_number, new_number);
+
+        new_number = secret_number * 2048;
+        mix_and_prune(&mut secret_number, new_number);
+
+        cache.insert(initial_number, secret_number);
+    }
+    secret_number
+}
+
+fn part1(seeds: &Vec<usize>, cache: &mut HashMap<usize, usize>) {
+    let mut secret_numbers: Vec<usize> = Vec::new();
+    for seed in seeds {
+        let secret_number = generate_secret_number(seed, cache);
+        secret_numbers.push(secret_number);
+    }
+
+    println!("{}", secret_numbers.iter().sum::<usize>());
+}
+
+fn generate_prices(seed: &usize, cache: &mut HashMap<usize, usize>) -> Vec<usize> {
+    let mut prices: Vec<usize> = Vec::new();
+    let mut secret_number = *seed;
+    let mut new_number: usize;
+    for _ in 0..2000 {
+        if let Some(number) = cache.get(&secret_number) {
+            secret_number = *number;
+            prices.push(secret_number % 10);
+            continue;
+        }
+        let initial_number = secret_number;
+
+        new_number = secret_number * 64;
+        mix_and_prune(&mut secret_number, new_number);
+
+        new_number = secret_number / 32;
+        mix_and_prune(&mut secret_number, new_number);
+
+        new_number = secret_number * 2048;
+        mix_and_prune(&mut secret_number, new_number);
+
+        prices.push(secret_number % 10);
+
+        cache.insert(initial_number, secret_number);
+    }
+    prices
+}
+
+fn part2(seeds: &Vec<usize>, cache: &mut HashMap<usize, usize>) {
+    let mut changes_map: HashMap<(isize, isize, isize, isize), HashMap<usize, usize>> =
+        HashMap::new();
+    for seed in seeds {
+        let prices = generate_prices(seed, cache);
+        let mut current_price = prices[0];
+        let mut changes: Vec<isize> = vec![current_price as isize];
+        for i in 0..prices.len() - 1 {
+            let change = (prices[i + 1] as isize) - (current_price as isize);
+            changes.push(change);
+            current_price = prices[i + 1]
+        }
+
+        for i in 1..changes.len() - 3 {
+            let value = prices[i + 3];
+            let sequence = (changes[i], changes[i + 1], changes[i + 2], changes[i + 3]);
+            let map_for_sequence = changes_map.entry(sequence).or_default();
+
+            if let None = map_for_sequence.get(seed) {
+                map_for_sequence.insert(*seed, value);
+            }
+        }
+    }
+
+    let valid_sequences = changes_map
+        .iter()
+        .map(|(sequence, seq_map)| (*sequence, seq_map.iter().map(|(_, &value)| value).sum()))
+        .collect::<Vec<((isize, isize, isize, isize), usize)>>();
+    // println!("{:?}", changes_map);
+    println!(
+        "{:?}",
+        valid_sequences.iter().max_by(|x, y| x.1.cmp(&y.1)).unwrap()
+    );
+}
+
 pub fn day22(contents: &str) {
-    todo!()
+    let seeds = parse(contents);
+    let mut cache: HashMap<usize, usize> = HashMap::new();
+    part1(&seeds, &mut cache);
+    part2(&seeds, &mut cache);
 }
